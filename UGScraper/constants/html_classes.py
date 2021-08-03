c8e44a316b67d1f6d024b869c8b87525c81981de
dd-dreams <yd.nuk@nukd.one>
2021-08-03 14:06:44 +0300

    Changed way to access constants

---
+++ UGScraper/constants/html_classes.py
@@ -0,0 +1,6 @@
+
+ALL_SONGS_CLASS = "_3uKbA"
+RATING_CLASS_NAME = "_2amQf _3LNtq"
+LINK_NAME_CLASS = "_3DU-x JoRLr _3dYeW"
+CHORDS_CLASS = "_3cXAr _1G5k-"
+TYPE_CLASS = "_2amQf _2Fdo4"
---
+++ UGScraper/constants/messages.py
@@ -0,0 +1,27 @@
+
+FINDING_SONGS = "Finding songs..."
+SONGS_NOT_FOUND = "Songs have not been found. Make sure you typed the song currectly."
+SONGS_FOUND = "Songs have been found. Showing results..."
+FETCHING_RESULTS = "Fetching results"
+NO_SONG_SET = "No song to search"
+NO_ARTIST_SET = "No specified artist"
+SUCCESS_SCRAPE = "Successfully scraped"
+SUCCESS_CHORDS = "Successfully output chords"
+ALREADY_SCRAPED = "You already scraped this song. Check in cached_sites folder."
+OUTPUTTING_MSG = "Outputting chords"
+FILE_NOT_EXIST = "You didn't scrape or output, try again after you did."
+DESCRIPTION = """
+Scrape Ultimate Guitar website with no tracking (less at least), fast and easy, and lightweight.
+You can use the shell provided with the program, or parse arguments and get instant results.
+Enjoy!
+
+"""
+HELP_MSG = """song=, specify song
+artist=, specify artist
+scrape, scrape the website
+options, show current options
+output, output the chords
+bye/exit/quit, exits the program
+open, opens the HTML file after being scraped
+"""
+NO_GECKODRIVER_ERROR = "No geckodriver was found. Specify he's location by using --geckodriver"
